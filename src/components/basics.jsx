08a9e82bf1277d2c540e60e9d8ece85dc187badc
Aniket Agrawal <aniket.mpa@gmail.com>
2023-08-31 20:49:17 +0200

    Allow section to be edited or submitted

--- src/components/basics.jsx
+++ src/components/basics.jsx
@@ -8,6 +8,7 @@ function Basics() {
         "email" : "Email@Domain"
     };
     const [basicInfo, setBasicInfo] = useState(initialObj);
+    const [formDisplayed, setFormDisplayed] = useState(true);
 
     function handleChange(newObj) {
         setBasicInfo(
@@ -15,41 +16,59 @@ function Basics() {
         );
     }
 
-    return (
-        <section className = "basics">
-            <label htmlFor = "name">Name: </label>
-            <input
-                placeholder = "name"
-                type = "text"
-                id = "name"
-                value = {basicInfo.name}
-                onChange = {(e) => handleChange({
-                        ...basicInfo,
-                        "name" : e.target.value
-                    })
-                } />
-            <label htmlFor = "phone">Phone: </label>
-            <input
-                type = "text"
-                id = "phone"
-                value = {basicInfo.phone}
-                onChange = {(e) => handleChange({
-                        ...basicInfo,
-                        "phone" : e.target.value
-                    })
-                } />
-            <label htmlFor = "email">Email: </label>
-            <input
-                type = "email"
-                id = "email"
-                value = {basicInfo.email}
-                onChange = {(e) => handleChange({
-                        ...basicInfo,
-                        "email" : e.target.value
-                    })
-                } />
-        </section>
-    );
+    function handleClick(e) {
+        e.preventDefault();
+        setFormDisplayed(!formDisplayed);
+    }
+
+    if (formDisplayed) {
+        return (
+            <section className = "basics">
+                <label htmlFor = "name">Name: </label>
+                <input
+                    placeholder = "name"
+                    type = "text"
+                    id = "name"
+                    value = {basicInfo.name}
+                    onChange = {(e) => handleChange({
+                            ...basicInfo,
+                            "name" : e.target.value
+                        })
+                    } />
+                <label htmlFor = "phone">Phone: </label>
+                <input
+                    type = "text"
+                    id = "phone"
+                    value = {basicInfo.phone}
+                    onChange = {(e) => handleChange({
+                            ...basicInfo,
+                            "phone" : e.target.value
+                        })
+                    } />
+                <label htmlFor = "email">Email: </label>
+                <input
+                    type = "email"
+                    id = "email"
+                    value = {basicInfo.email}
+                    onChange = {(e) => handleChange({
+                            ...basicInfo,
+                            "email" : e.target.value
+                        })
+                    } />
+                <button onClick = {handleClick}>{formDisplayed ? "Submit" : "Edit"}</button>
+            </section>
+        );
+    } 
+    else {
+        return (
+            <section className = "basics">
+                <p>Name: {basicInfo.name}</p>
+                <p>Phone: {basicInfo.phone}</p>
+                <p>Email: {basicInfo.email}</p>
+                <button onClick = {handleClick}>{formDisplayed ? "Submit" : "Edit"}</button>
+            </section>
+        );
+    }    
 }
 
 export {Basics};
