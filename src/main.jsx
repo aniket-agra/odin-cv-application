62fbcaae918c896ab1e22da8a468093b76b14e1b
Aniket Agrawal <aniket.mpa@gmail.com>
2023-09-02 13:43:52 +0200

    Update paths for imports

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
 import { Basics } from './components/basics.jsx'
 import { EducationSection } from './components/education.jsx'
-import { WorkExSection } from '../components/work.jsx'
+import { WorkExSection } from './components/work.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
